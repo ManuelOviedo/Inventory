c450c3466b667534c60c2db488465646538d5ddd
Jose Manuel Oviedo <jose.manuel.oviedo.guzman@gmail.com>
2019-11-01 13:41:20 -0700

    First commit

---
+++ Inventario/Login.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Inventario
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public String strArr { get; set; }
+        public Login()
+        {
+            DataContext = this;
+            InitializeComponent();
+            strArr = "Hey there!";
+        }
+
+        void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+        void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
